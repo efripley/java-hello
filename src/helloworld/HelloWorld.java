fedcaf3b5d521c9e1a6e877c784e174314aabbbc
Ernie Ripley <efripley@gmail.com>
2016-09-08 12:25:55 -0600

    bitwise opperators

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -35,5 +35,62 @@ public class HelloWorld {
 
     	assert 17 % 5 == 2;
     	assert -7 % 5 == -2;
+
+        int a = 1;
+        ++a;
+
+        try(Close out = outExpect("2")){
+            println(a);
+        }
+
+        try(Close out = outExpect("3")){
+            println(++a);
+        }
+
+        try(Close out = outExpect("3")){
+            println(a);
+        }
+
+         try(Close out = outExpect("3")){
+            println(a++);
+        }
+
+        assert (~0b0000_0000_0000_0000_0000_0000_1111_0000) == 0b1111_1111_1111_1111_1111_1111_0000_1111; //bitwise not (flips)
+
+        assert (0b1111_0000 & 0b1010_1010) == 0b1010_0000; //bitwise and (if either value is false[0] then result will be false[0])
+
+        assert (0b1111_0000 | 0b1010_1010) == 0b1111_1010; //bitwise or (if either value is true[1] then result will be true[1])
+
+        assert (0b1010_1111 >> 4) == 0b0000_1010; //bitshift right shifts the sign bit in
+
+        assert (0b1010_1111 << 4) == 0b1010_1111_0000; //bitshift left
+
+        println(a);
+
+        int b = random(0, 1000000);
+
+        println(b);
+
+        int c = random(-1000000, 1000000);
+
+        println(c);
+
+        assert ((~c) + 1) == -c;
+
+        println(Integer.toString(-2, 2));
+
+        assert (b >> 1) == b / 2;
+        assert (b << 1) == b * 2;
+
+        assert -2 == 0b1111_1111_1111_1111_1111_1111_1111_1110;
+    }
+
+    void testConvert(){
+        byte x = -1;
+        assert x == (byte)0b1111_1111;
+
+        int y = x;
+
+        assert y == -1;
     }
 }
